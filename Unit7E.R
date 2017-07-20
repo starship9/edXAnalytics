c6b1b581a5d463ed718d5aa49d2dbca6991b71e0
starship9 <nishanksaini10@gmail.com>
2017-07-21 01:03:15 +0530

    Household trends

--- Unit7E.R
+++ Unit7E.R
@@ -1,2 +1,9 @@
 households <- read.csv("C:/Users/Nishank/Desktop/SNU/RStuff/households.csv")
 str(households)
+
+library(ggplot2)
+library(reshape2)
+
+head(melt(households, id = "Year"))
+
+ggplot(data = melt(households, id = "Year"),mapping = aes(Year, y = value, color = variable)) + geom_line(size = 2) + geom_point(size = 5) + ylab("Percentage of households")
